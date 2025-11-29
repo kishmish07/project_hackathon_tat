8dcaf6788e391811b5053af50653c2058928c440
Mishen735 <151943024+735812@users.noreply.github.com>
2025-11-30 00:51:35 +0300

    Update example.py

--- notebooks/example.py
+++ notebooks/example.py
@@ -1,11 +1,12 @@
 import pandas as pd
 from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
 from tqdm import tqdm
-import torch
-model_name = "bigscience/mt0-small"
-test_file = "/Users/karim/Downloads/Датасет_для_обучения_модели_Аскар.csv"
+test_file = "test.tsv"
 df_test = pd.read_csv(test_file, sep="\t")
 df_test.columns = df_test.columns.str.strip()
+assert "ID" in df_test.columns
+assert "tat_toxic" in df_test.columns
+model_name = "bigscience/mt0-small"
 tokenizer = AutoTokenizer.from_pretrained(model_name)
 model = AutoModelForSeq2SeqLM.from_pretrained(model_name)
 model.to("cpu")
@@ -13,5 +14,5 @@ def detox_tatar(text):
     inputs = tokenizer(f"Detoxify this Tatar text: {text}", return_tensors="pt", truncation=True)
     outputs = model.generate(**inputs, max_length=128, num_beams=4, no_repeat_ngram_size=4)
     return tokenizer.decode(outputs[0], skip_special_tokens=True)
-df_test["detox"] = [detox_tatar(t) for t in tqdm(df_test["toxic"], desc="Detoxifying")]
-df_test[["toxic", "detox"]].to_csv("submission.tsv", sep="\t", index=False)
+df_test["tat_detox1"] = [detox_tatar(t) for t in tqdm(df_test["tat_toxic"], desc="Detoxifying")]
+df_test[["ID", "tat_toxic", "tat_detox1"]].to_csv("submission.tsv", sep="\t", index=False)
